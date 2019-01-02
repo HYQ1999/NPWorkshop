96795b79ff2c9d507e87528cffbe1b048bcd5483
xuzhouxu <963915882@qq.com>
2019-01-02 09:12:52 +0800

    报修首页布局

---
+++ NPWorkshop/NPWorkshop/Baoxiu/LeftViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LeftViewController.swift
+//  NPWorkshop
+//
+//  Created by 周旭 on 2018/12/29.
+//  Copyright © 2018年 韩意谦. All rights reserved.
+//
+
+import UIKit
+
+class LeftViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Left Menu"
+        self.view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
