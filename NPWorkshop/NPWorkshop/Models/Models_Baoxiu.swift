12b7483028c8cd5d7a67e946a6be9f3b978b556f
xuzhouxu <963915882@qq.com>
2019-01-14 08:18:00 +0800

    图片文件上传

---
+++ NPWorkshop/NPWorkshop/Models/Models_Baoxiu.swift
@@ -0,0 +1,14 @@
+//
+//  Models_Baoxiu.swift
+//  NPWorkshop
+//
+//  Created by 周旭 on 2019/1/13.
+//  Copyright © 2019年 韩意谦. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+class Models_Baoxiu: NSObject {
+
+}
